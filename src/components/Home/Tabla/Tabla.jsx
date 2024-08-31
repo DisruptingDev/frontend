1421006a83bca1a8530a87bdedc0baf344d7cbb5
HugoBaez <hugobg24@gmail.com>
2024-08-30 20:29:05 -0600

    Tablas con filtros, Columna Serie y formato dinero

--- src/components/Home/Tabla/Tabla.jsx
+++ src/components/Home/Tabla/Tabla.jsx
@@ -15,9 +15,18 @@ import {
   TextField,
 } from '@mui/material';
 
-function createData(id, folio, emisor, receptor, estatus, subtotal, traslados, retenciones, total, usuario) {
-  return { id, folio, emisor, receptor, estatus, subtotal, traslados, retenciones, total, usuario };
+function createData(id, folio, emisor, receptor, serie, estatus, subtotal, traslados, retenciones, total, usuario) {
+  return { id, folio, emisor, receptor, serie, estatus, subtotal, traslados, retenciones, total, usuario };
 }
+
+// Función para formatear como moneda
+const formatCurrency = (value) => {
+  return new Intl.NumberFormat('es-MX', {
+    style: 'currency',
+    currency: 'MXN',
+    minimumFractionDigits: 2,
+  }).format(value);
+};
 
 export default function DataTable() {
   const [rows, setRows] = useState([]);
@@ -46,6 +55,7 @@ export default function DataTable() {
               item.Folio,
               item.Emisor.Nombre || 'Desconocido',
               item.Receptor.Nombre || 'Desconocido',
+              item.Serie,
               'Timbrada',
               item.SubTotal,
               item.Conceptos?.TotalImpuestosTrasladados || 0,
@@ -97,27 +107,28 @@ export default function DataTable() {
   return (
     <Box bgcolor="white" mx={4} p={4} boxShadow={3} borderRadius={2}>
       <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-        <TableContainer>
+        <TableContainer align='center'>
           <Table sx={{ minWidth: 650 }} aria-label="customized table">
             <TableHead>
               <TableRow sx={{ backgroundColor: '#04b2ca' }}>
-                <TableCell padding="checkbox">
+                <TableCell padding="checkbox" sx={{ textAlign: 'center' }}>
                   {/* <Checkbox color="primary" /> */}
                 </TableCell>
-                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white' }}>ID</TableCell>
-                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white' }}>Folio</TableCell>
-                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white' }}>Emisor</TableCell>
-                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white' }}>Receptor</TableCell>
-                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white' }}>Estatus</TableCell>
-                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white' }}>Subtotal</TableCell>
-                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white' }}>Traslados</TableCell>
-                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white' }}>Retenciones</TableCell>
-                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white' }}>Total</TableCell>
-                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white' }}>Usuario</TableCell>
+                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white', textAlign: 'center' }}>ID</TableCell>
+                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white', textAlign: 'center' }}>Folio</TableCell>
+                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white', textAlign: 'center' }}>Emisor</TableCell>
+                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white', textAlign: 'center' }}>Receptor</TableCell>
+                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white', textAlign: 'center' }}>Serie</TableCell>
+                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white', textAlign: 'center' }}>Estatus</TableCell>
+                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white', textAlign: 'center' }}>Subtotal</TableCell>
+                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white', textAlign: 'center' }}>Traslados</TableCell>
+                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white', textAlign: 'center' }}>Retenciones</TableCell>
+                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white', textAlign: 'center' }}>Total</TableCell>
+                <TableCell sx={{ fontSize: '1rem', fontWeight: 'bold', color: 'white', textAlign: 'center' }}>Usuario</TableCell>
               </TableRow>
-              <TableRow>
-                <TableCell padding="checkbox"></TableCell>
-                <TableCell>
+              <TableRow sx={{ padding: 0 }}>
+                <TableCell padding="checkbox" sx={{ textAlign: 'center' }}></TableCell>
+                <TableCell sx={{ padding: 0 }} align='center'>
                   <TextField
                     variant="outlined"
                     size="small"
@@ -127,11 +138,12 @@ export default function DataTable() {
                     placeholder="ID"
                     fullWidth
                     sx={{
-                      width: { xs: '60px', sm: '80px', md: '50px' }, // Ancho responsivo
+                      width: { xs: '60px', sm: '80px', md: '80px' }, // Ancho responsivo
+             
                     }}
                   />
                 </TableCell>
-                <TableCell>
+                <TableCell sx={{ px: "1" }} align='center'>
                   <TextField
                     variant="outlined"
                     size="small"
@@ -141,12 +153,11 @@ export default function DataTable() {
                     placeholder="Folio"
                     fullWidth
                     sx={{
-                      width: { xs: '80px', sm: '100px', md: '80px' }, // Ancho responsivo
-
+                      width: { xs: '80px', sm: '100px', md: '140px' }, // Ancho responsivo
                     }}
                   />
                 </TableCell>
-                <TableCell>
+                <TableCell align='center' sx={{ padding: 0 }}>
                   <TextField
                     variant="outlined"
                     size="small"
@@ -155,26 +166,27 @@ export default function DataTable() {
                     onChange={handleFilterChange}
                     placeholder="Emisor"
                     fullWidth
-                    sx={{
-                      width: { xs: '100px', sm: '120px', md: '140px' }, // Ancho responsivo
-                    }}
+                    // sx={{
+                    //   width: { xs: '100px', sm: '120px', md: '120px' }, // Ancho responsivo
+                    // }}
                   />
                 </TableCell>
-                <TableCell>
+                <TableCell align='center' sx={{ padding: 0 }}> 
                   <TextField
                     variant="outlined"
-                    // size="small"
+                    size="small"
                     name="receptor"
                     value={filters.receptor}
                     onChange={handleFilterChange}
                     placeholder="Receptor"
-                    // fullWidth
-                    sx={{
-                      width: { xs: '100px', sm: '120px', md: '140px' }, // Ancho responsivo
-                      marginBottom: '4px',
-                    }}
+                    fullWidth
+                    // sx={{
+                    //   width: { xs: '100px', sm: '120px', md: '150px' }, // Ancho responsivo
+                    //   marginBottom: '4px',
+                    // }}
                   />
                 </TableCell>
+                <TableCell></TableCell>
                 <TableCell></TableCell>
                 <TableCell></TableCell>
                 <TableCell></TableCell>
@@ -188,19 +200,20 @@ export default function DataTable() {
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row) => (
                   <TableRow key={row.id}>
-                    <TableCell padding="checkbox">
+                    <TableCell padding="checkbox" sx={{ textAlign: 'center' }}>
                       <Checkbox color="primary" />
                     </TableCell>
-                    <TableCell>{row.id}</TableCell>
-                    <TableCell>{row.folio}</TableCell>
-                    <TableCell>{row.emisor}</TableCell>
-                    <TableCell>{row.receptor}</TableCell>
-                    <TableCell>{row.estatus}</TableCell>
-                    <TableCell>{row.subtotal}</TableCell>
-                    <TableCell>{row.traslados}</TableCell>
-                    <TableCell>{row.retenciones}</TableCell>
-                    <TableCell>{row.total}</TableCell>
-                    <TableCell>{row.usuario}</TableCell>
+                    <TableCell sx={{ textAlign: 'center' }}>{row.id}</TableCell>
+                    <TableCell sx={{ textAlign: 'center' }}>{row.folio}</TableCell>
+                    <TableCell sx={{ textAlign: 'center' }}>{row.emisor}</TableCell>
+                    <TableCell sx={{ textAlign: 'center' }}>{row.receptor}</TableCell>
+                    <TableCell sx={{ textAlign: 'center' }}>{row.serie}</TableCell>
+                    <TableCell sx={{ textAlign: 'center' }}>{row.estatus}</TableCell>
+                    <TableCell sx={{ textAlign: 'center' }}>{formatCurrency(row.subtotal)}</TableCell>
+                    <TableCell sx={{ textAlign: 'center' }}>{formatCurrency(row.traslados)}</TableCell>
+                    <TableCell sx={{ textAlign: 'center' }}>{formatCurrency(row.retenciones)}</TableCell>
+                    <TableCell sx={{ textAlign: 'center' }}>{formatCurrency(row.total)}</TableCell>
+                    <TableCell sx={{ textAlign: 'center' }}>{row.usuario}</TableCell>
                   </TableRow>
                 ))}
             </TableBody>
