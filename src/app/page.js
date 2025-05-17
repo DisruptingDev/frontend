35d03798ca42950a8574ecf97596ee64491922ec
Desarrollo <contacto@mauriciocasado.com>
2025-05-16 18:02:49 -0600

    cambios front cards y paquetes

--- src/app/page.js
+++ src/app/page.js
@@ -17,9 +17,9 @@ export default function HomePage() {
         <div>
         <AppAppBar/>
         <Hero />
+        <Featured/>
         <Functions/>
         <Highlights/>
-        {/*<Featured/>*/}
         <Pricing/> 
         <Footer />
         </div>
