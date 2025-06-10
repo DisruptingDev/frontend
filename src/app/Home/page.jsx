805df259f25a4da1d6bc7582ab27ea620c16b655
Edwingzmn <eguzmann1601@alumno.ipn.mx>
2025-06-10 11:20:26 -0600

    ocultar wizard

--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -32,7 +32,7 @@ export default function Home() {
 
     //Nuevo usuario
     const [newUser, setNewUser] = useState(null);
-    const [openWizard, setOpenWizard] = useState(true);
+    const [openWizard, setOpenWizard] = useState(false);
     const [setOpen] = useState(false);
 
     // Estados para manejar el token de autenticación y el filtro de búsqueda
