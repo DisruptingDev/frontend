77552c282a0c1ddc57ef3efb34be173f644ea733
Edwin Guzmán <eguzmann1601@alumno.ipn.mx>
2026-01-08 13:11:45 -0600

    Fix en edición de pagos

--- src/components/FormFactura/Receptor/Receptor.jsx
+++ src/components/FormFactura/Receptor/Receptor.jsx
@@ -67,7 +67,7 @@ export default function Receptor({ register, watch, lugarExpedicion, getValues, 
 
             if (rfc === "XAXX010101000") {
 
-                setValue("Año", receptorData.InformacionGlobal.Año);
+                setValue("Anio", receptorData.InformacionGlobal.Anio);
                 setValue("Meses", receptorData.InformacionGlobal.Meses);
                 setValue("Periodicidad", receptorData.InformacionGlobal.Periodicidad);
 
@@ -424,7 +424,7 @@ export default function Receptor({ register, watch, lugarExpedicion, getValues, 
 
                         label="Año"
                         type="number"
-                        {...register("Año")}
+                        {...register("Anio")}
                         fullWidth
                         inputProps={{
                             min: 1900,
